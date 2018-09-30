23b8c900c17a390d02e8ee2a77b24d36f514c14b
Hiroyuki Miyazaki <miyazaki@mcommit.com>
2018-09-30 23:55:34 +0900

    add script for install clang, llvm

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
+# install clang, llvm
+sudo apt install clang-3.9 libclang-3.9-dev  llvm-3.9 llvm-3.9-dev
+
+# build
 clang++-3.9 autoc2.cpp $(llvm-config-3.9 --cxxflags) $(llvm-config-3.9 --ldflags --libs ) -lclangFrontend -lclangFrontendTool  -lclangParse -lclangSema -lclangEdit -lclangRewrite -lclangRewriteFrontend -lclangStaticAnalyzerFrontend -lclangStaticAnalyzerCheckers -lclangStaticAnalyzerCore -lclangSerialization -lclangFormat -lclangDriver -lclangTooling -lclangToolingCore -lclangAST -lclangLex -lclangAnalysis -lclangBasic
 
